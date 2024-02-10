b619849c5deeec15a39bec07f8df6bafd15ae9aa
LL-Nemesis-LL <153607610+LL-Nemesis-LL@users.noreply.github.com>
2024-02-10 15:02:00 +0100

    js AjoutQCM

---
+++ AjoutQCM/Back-end/SaveQuestion.php
@@ -0,0 +1,16 @@
+<?php
+//include "db_connect.php";
+$allowOrigin = "http://localhost";
+header("Access-Control-Allow-Origin: *");
+$question = htmlspecialchars($_POST["question"]);
+$response1 = htmlspecialchars($_POST["reponse1"]);
+$response2 = htmlspecialchars($_POST["reponse2"]);
+$response3 = htmlspecialchars($_POST["reponse3"]);
+$response4 = htmlspecialchars($_POST["reponse4"]);
+$responseTrue = htmlspecialchars($_POST["reponseTrue"]);
+$request = $db->prepare("INSERT INTO QCM VALUES(?, ?, ?, ?, ?, ?)");
+$request->bind_param("sssssi", $question, $response1, $response2, $response3, $response4, $responseTrue);
+$request->execute();
+$request->close();
+$db->close();
+?>
