74e3d903d34c5d2816ebe921f12decf86bef5642
Phoenix1 <130487661+Phoenix0108@users.noreply.github.com>
2024-02-14 10:55:15 +0100

    connection

--- asset/include/db_connect.php
+++ asset/include/db_connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost:3307";
 $username = "root";
 $password = "";
-$dbname = "formulaire";
+$dbname = "siteajax";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
--- asset/include/submit.php
+++ asset/include/submit.php
@@ -1,34 +1,26 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    include 'db_connect.php';
-    
-    // Collecte des données du formulaire
-    $name = $_POST['nom'];
-    $gender = $_POST['gender'];
-    $color = $_POST['color'];
-    $pet = implode(', ', $_POST['pet']);
+    // Récupérer les données du formulaire
+    $nomUtilisateur = $_POST["nom_utilisateur"];
+    $motDePasse = $_POST["mot_de_passe"];
 
+    // Vérifier les données dans la base de données
+    $requete = "SELECT * FROM Utilisateur WHERE NomUtilisateur = '$nomUtilisateur' AND MotDePasse = '$motDePasse'";
+    $resultat = mysqli_query($connexion, $requete);
 
-    if (empty($name) || empty($gender) || empty($color) || empty($pet)) {
-        echo "Toutes les cases sont nécessaires";
-    } else {
-        $sql = "INSERT INTO feedback (name, gender, color, pet) VALUES (?, ?, ?, ?)";
-        $stmt = $conn->prepare($sql);
-
-        // Liaison des valeurs aux paramètres avec les types corrects
-        $stmt->bind_param("ssss", $name, $gender, $color, $pet);
-
-        if ($stmt->execute()) {
-            // Rediriger après une opération réussie (redirection sûre via header)
-            header('Location: ../../merci.html');
+    if ($resultat) {
+        // Vérification des données
+        if (mysqli_num_rows($resultat) == 1) {
+            // L'utilisateur est authentifié, rediriger vers la page suivante
+            header("Location: page_suivante.php");
             exit();
         } else {
-            // Afficher un messgender d'erreur générique (sécurisé)
-            echo htmlspecialchars("$name, Une erreur s'est produite lors du traitement de votre demande. Veuillez réessayer plus tard.", ENT_QUOTES, 'UTF-8');
+            // Nom d'utilisateur ou mot de passe incorrect
+            $erreur = "Nom d'utilisateur ou mot de passe incorrect.";
         }
-
-        $stmt->close();
-        $conn->close();
+    } else {
+        // Erreur de requête
+        $erreur = "Erreur lors de la requête : " . mysqli_error($connexion);
     }
 }
 ?>
