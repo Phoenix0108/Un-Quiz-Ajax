787d00355bd38b99bc2aef3ccf1ce0e262428724
Phoenix1 <130487661+Phoenix0108@users.noreply.github.com>
2024-02-28 16:14:47 +0100

    Delete site directory

--- site/Acceuil/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $("#btnRecherche").click(function () {
-        window.location.href = "../RechercheQCM/rechercheqcm.html"
-    })
-    $("#btnAcces").click(function () {
-        window.location.href = "../MesQCM/mesqcm.html"
-    })
-})
